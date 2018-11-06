efd75ef50ce0d30100a6eec5b1a68fef393c27e3
Adil <muhammadrizwan5040@gmail.com>
2018-11-06 19:44:18 +0500

    done for now

---
+++ sendRequest/sendRequestVC.swift
@@ -0,0 +1,35 @@
+//
+//  sendRequestVC.swift
+//  Linkd
+//
+//  Created by Khawar on 11/6/18.
+//  Copyright © 2018 Khawar. All rights reserved.
+//
+
+import UIKit
+
+class sendRequestVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
