4bb62d0451112ec57b129bd4b2636e5bc793466d
Adil <muhammadrizwan5040@gmail.com>
2018-11-20 19:40:39 +0500

    20 november

---
+++ Link/Linkd-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Linkd-Bridging-Header.h
+//  Linkd
+//
+//  Created by Khawar on 11/13/18.
+//  Copyright © 2018 Khawar. All rights reserved.
+//
+
+#ifndef Linkd_Bridging_Header_h
+#define Linkd_Bridging_Header_h
+
+
+#endif /* Linkd_Bridging_Header_h */
---
+++ Request/requestModelClass.swift
@@ -0,0 +1,9 @@
+//
+//  requestModelClass.swift
+//  Linkd
+//
+//  Created by Khawar on 11/20/18.
+//  Copyright © 2018 Khawar. All rights reserved.
+//
+
+import Foundation
